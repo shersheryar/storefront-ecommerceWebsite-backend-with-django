983a60eb0c181237e8fdea6e13c2c00ef4dc47bc
shersheryar <p218027@pwr.nu.edu.pk>
2024-08-15 17:13:06 +0500

    templates in django

--- playground/views.py
+++ playground/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 
 
 def say_hello(request):
-    return HttpResponse("hello world")
+    # return HttpResponse("hello world")
+    return render(request, "hello.html", {'name': "Sheryar"})
