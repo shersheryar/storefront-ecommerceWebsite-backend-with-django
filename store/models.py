d152eefa6d78cc22df6c550dd2f0e683f4f6c423
shersheryar <p218027@pwr.nu.edu.pk>
2024-08-16 15:44:20 +0500

    addes membership attribute

--- store/models.py
+++ store/models.py
@@ -12,8 +12,19 @@ class Product(models.Model):
 
 
 class Customer(models.Model):
+    MEMEBERSHIP_BRONZE = 'B'
+    MEMEBERSHIP_SILVER = 'S'
+    MEMEBERSHIP_GOLD = 'G'
+
+    MEMEBERSHIP_CHOICES = [
+        (MEMEBERSHIP_BRONZE, 'Bronze'),
+        (MEMEBERSHIP_SILVER, 'Silver'),
+        (MEMEBERSHIP_GOLD, 'Gold'),
+    ]
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=20)
     birth_date = models.DateField(null=True)
+    membership = models.CharField(
+        max_length=1, choices=MEMEBERSHIP_CHOICES, default=MEMEBERSHIP_BRONZE)
