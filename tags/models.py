b81e986ad86002dfca93483a90dda48f28a43bb2
shersheryar <p218027@pwr.nu.edu.pk>
2024-09-29 16:15:22 +0500

    added models

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 # Create your models here.
+
+
+class Tag(models.Model):
+
+    label = models.CharField(max_length=255)
+
+
+class TaggedItem(models.Model):
+    # what tag applied to what product
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE) 
+    # first we need to know type(product, video)
+    # then Id =
+    # by using both we can find any item
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
