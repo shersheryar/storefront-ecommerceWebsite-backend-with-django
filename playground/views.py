0b8244260319da26850082a32dde2370bbce1730
shersheryar <p218027@pwr.nu.edu.pk>
2024-10-02 02:26:51 +0500

    handle if the data is not in database

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 from store.models import Customer
 # Create your views here.
 
@@ -7,9 +8,17 @@ from store.models import Customer
 def say_hello(request):
     # return HttpResponse("hello world")
     # objects perform as an interface to the database
-    # help to interact with the database without writing SQL queries directly to the database 
-    #  ORM (Object Relational Mapping) is a programming technique that allows developers to interact with a relational database using an object-oriented programming language. 
-    query_set = Customer.objects.all()
-    for x in query_set:
-        print(x.first_name)
+    # help to interact with the database without writing SQL queries directly to the database
+    #  ORM (Object Relational Mapping) is a programming technique that allows developers to interact with a relational database using an object-oriented programming language.
+
+    # retrieve all the records from the database
+    try:
+        customer = Customer.objects.get(pk=0)
+    except ObjectDoesNotExist:
+        return HttpResponse("Customer does not exist")
+    # customer = Customer.objects.first()
+    print(customer.first_name)
+    # query_set = Customer.objects.all()
+    # for x in query_set:
+    #     print(x.first_name)
     return render(request, "hello.html", {'name': "Sheryar"})
