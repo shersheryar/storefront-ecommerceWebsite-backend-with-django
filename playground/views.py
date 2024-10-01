7fc0ef8c407f853f592a65b5e4c4a8e1d6e2957d
shersheryar <p218027@pwr.nu.edu.pk>
2024-10-02 02:17:44 +0500

    manager and querysets

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Customer
 # Create your views here.
 
 
 def say_hello(request):
     # return HttpResponse("hello world")
+    # objects perform as an interface to the database
+    # help to interact with the database without writing SQL queries directly to the database 
+    #  ORM (Object Relational Mapping) is a programming technique that allows developers to interact with a relational database using an object-oriented programming language. 
+    query_set = Customer.objects.all()
+    for x in query_set:
+        print(x.first_name)
     return render(request, "hello.html", {'name': "Sheryar"})
