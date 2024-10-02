cbeecb066845dbeb79d38b076f6b05816c03269a
shersheryar <p218027@pwr.nu.edu.pk>
2024-10-03 00:00:20 +0500

    implemented complex queries

--- playground/views.py
+++ playground/views.py
@@ -6,19 +6,26 @@ from store.models import Customer
 
 
 def say_hello(request):
-    # return HttpResponse("hello world")
+
     # objects perform as an interface to the database
     # help to interact with the database without writing SQL queries directly to the database
     #  ORM (Object Relational Mapping) is a programming technique that allows developers to interact with a relational database using an object-oriented programming language.
 
     # retrieve all the records from the database
-    try:
-        customer = Customer.objects.get(pk=0)
-    except ObjectDoesNotExist:
-        return HttpResponse("Customer does not exist")
+    # try:
+    #     customer = Customer.objects.get(pk=0)
+    # except ObjectDoesNotExist:
+    #     return HttpResponse("Customer does not exist")
     # customer = Customer.objects.first()
-    print(customer.first_name)
+
+    # filtering data
+    # customers = Customer.objects.filter(id__range=(1, 3))
+
+    # complex filtering
+    customers = Customer.objects.filter(
+        id__gt=2).filter(first_name__startswith='A')
+    print(list(customers))
     # query_set = Customer.objects.all()
     # for x in query_set:
     #     print(x.first_name)
-    return render(request, "hello.html", {'name': "Sheryar"})
+    return render(request, "hello.html", {'customers': list(customers)})
