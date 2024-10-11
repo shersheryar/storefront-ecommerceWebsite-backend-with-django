e24ba0c3aec39b68b8e7f12f7a0f748102d69dd0
shersheryar <p218027@pwr.nu.edu.pk>
2024-10-11 22:01:47 +0500

    admin site started

--- storefront/urls.py
+++ storefront/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
 
+admin.site.site_header = "Storefront Admin"
+admin.site.site_title = "Storefront Admin Portal"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('playground/', include('playground.urls'))
